64254902eac64724c9e88acd83ce6d8e7bf3489c
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-10-04 19:00:27 -0700

    chain: remove redundant deps and imports
    hardhat-toolbox includes gas-reporter

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
-import 'hardhat-gas-reporter'
 import 'hardhat-tracer'
 
 const config: HardhatUserConfig = {
