505cfca869be09bbedbec6dbf89057755b0201f7
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-10-09 15:02:11 -0700

    client: setting up for connected and not connected

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,13 @@
 import { Router, Route } from 'preact-router'
 import { Home } from './views/home/Home'
+import { NotConnected } from './views/notConnected/NotConnected'
 
 export function App() {
+  const isConnected = true
+
   return (
     <Router>
-      <Route path='/' component={Home} />
+      <Route path='/' component={isConnected ? Home : NotConnected} />
     </Router>
   )
 }
---
+++ client/src/views/notConnected/NotConnected.tsx
@@ -0,0 +1,9 @@
+import { Flex, Heading } from '@chakra-ui/react'
+
+export const NotConnected = () => {
+  return (
+    <Flex width='100%'>
+      <Heading>Not Connected</Heading>
+    </Flex>
+  )
+}
