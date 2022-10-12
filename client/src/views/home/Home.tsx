7d557013e7ae091d3ea297dcbb371898a6b662b1
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-10-11 19:10:48 -0700

    client: basic layout for Home

--- client/src/views/home/Home.tsx
+++ client/src/views/home/Home.tsx
@@ -1,13 +1,20 @@
-import { Container, Flex, Heading, Stack } from '@chakra-ui/react'
+import { Center, Container, Heading } from '@chakra-ui/react'
 import { useMetaMask } from 'metamask-react'
 
 export const Home = () => {
   const { account } = useMetaMask()
 
   return (
-    <Container maxW='5xl' width='100%' direction='column' align='center'>
-      <Heading>Home</Heading>
+    <Container maxW='7xl' width='100%' direction='column' align='center'>
+      <Heading>This is your Home</Heading>
+
+      <Heading>This is your account:</Heading>
       <Heading>{account}</Heading>
+
+      <Center>Give Permission to Account</Center>
+      <Center>Accounts you have permission from</Center>
+      <Center>Accounts you have given permission to</Center>
+      <Center>Your Wall</Center>
     </Container>
   )
 }
