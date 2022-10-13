1a5459f5afd5a016675fe6f9c1b268829efe2804
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-10-12 18:50:38 -0700

    client: setting up grantPermission call

--- client/src/views/home/GivePermission.tsx
+++ client/src/views/home/GivePermission.tsx
@@ -1,10 +1,24 @@
 import { Box, Button, Container, Input } from '@chakra-ui/react'
 import { ethers } from 'ethers'
+import { useMetaMask } from 'metamask-react'
 import { useState } from 'preact/hooks'
+
+const contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+const abi = ['function grantPermission(address target) public']
+
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+const signer = provider.getSigner()
+
+const contract = new ethers.Contract(contractAddress, abi, provider)
+
+const contractWithSigner = contract.connect(signer)
 
 export const GivePermission = () => {
   const [isAddressValid, setIsAddressValied] = useState(true)
   const [address, setAddress] = useState('')
+
+  const { ethereum } = useMetaMask()
 
   const handleInputChange = (event: any) => {
     const { value } = event.target
@@ -15,6 +29,31 @@ export const GivePermission = () => {
     } else {
       setIsAddressValied(false)
     }
+  }
+
+  const handleGivePermission = async () => {
+    const contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+    const abi = ['function grantPermission(address target) public']
+
+    const provider = new ethers.providers.Web3Provider(ethereum)
+
+    console.log({ provider })
+
+    const signer = provider.getSigner(0)
+
+    console.log({ signer })
+
+    const contract = new ethers.Contract(contractAddress, abi, provider)
+
+    console.log({ contract })
+
+    const contractWithSigner = contract.connect(signer)
+
+    console.log({ contractWithSigner })
+
+    console.log('give permission to', address)
+    const result = await contractWithSigner.grantPermission(address)
+    console.log({ result })
   }
 
   return (
@@ -32,7 +71,10 @@ export const GivePermission = () => {
       {!!address.length && !isAddressValid && (
         <Box color='red.500'>Address is not valid</Box>
       )}
-      <Button disabled={!address.length || !isAddressValid}>
+      <Button
+        onClick={handleGivePermission}
+        disabled={!address.length || !isAddressValid}
+      >
         Give Permission
       </Button>
     </Container>
